9fde39a5f9d5430de6b239dd2306b9824c809c30
王永杰 <xiayuexingkong@gmail.com>
2023-06-08 15:57:12 +0800

    创建GlideStudy repository

---
+++ app/src/main/java/com/wyj/glide/GlideActivity.kt
@@ -0,0 +1,48 @@
+package com.wyj.glide
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.cache.DiskCache
+import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class GlideActivity : AppCompatActivity() {
+    companion object {
+        // 静态图片资源
+        private const val URL = "https://cn.bing.com/sa/simg/hpb/LaDigue_EN-CA1115245085_1920x1080.jpg"
+//        private const val URL = ""
+
+        // Gif资源
+        private const val GIF_URL = "http://p1.pstatp.com/large/166200019850062839d3"
+    }
+
+    private var ivGlide: ImageView? = null;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_glide)
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
+            load()
+        }
+
+        ivGlide = findViewById<ImageView>(R.id.ivGlide)
+
+
+    }
+
+    private fun load() {
+        val options = RequestOptions()
+            .placeholder(ColorDrawable(Color.GRAY))
+            .error(ColorDrawable(Color.RED))
+            .fallback(ColorDrawable(Color.CYAN))
+            .skipMemoryCache(true)
+            .diskCacheStrategy(DiskCacheStrategy.NONE)
+        Glide.with(this).load(URL).apply(options).into(ivGlide!!)
+    }
+}
